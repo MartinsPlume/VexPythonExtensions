8bfe83e0662349999aebd68efd5e137cc5f57854
Mārtiņš Plūme <56507599+MartinsPlume@users.noreply.github.com>
2024-03-28 16:39:02 +0200

    Update driveTrainExended.py

--- driveTrainExended.py
+++ driveTrainExended.py
@@ -1,14 +1,19 @@
 from vex import *
 
-class driveTrainExtended(DriveTrain):
-    def __init__(self, lm, rm, wheelTravel=300, trackWidth=320, wheelBase=320,
-                 units=DistanceUnits.MM, externalGearRatio=1.0):
-        super().__init__(lm, rm, wheelTravel, trackWidth, wheelBase, units, externalGearRatio)
-    
-    def turn_slightly(self, direction, speed):
+def turn_slightly(self, direction, speed):
         if direction == TurnType.LEFT:
             self.lm.spin(FORWARD, 100 - speed, PERCENT)
             self.rm.spin(FORWARD)
         elif direction == TurnType.RIGHT:
             self.rm.spin(FORWARD, 100 - speed, PERCENT)
             self.lm.spin(FORWARD)
+
+class DriveTrainExtended(DriveTrain):
+    def __init__(self, lm, rm, wheelTravel=300, trackWidth=320, wheelBase=320,
+                 units=DistanceUnits.MM, externalGearRatio=1.0):
+        super().__init__(lm, rm, wheelTravel, trackWidth, wheelBase, units, externalGearRatio)
+    
+class SmartDriveExtended(SmartDrive):
+    def __init__(self, lm, rm, g, wheelTravel=300, trackWidth=320, wheelBase=320,
+                 units=DistanceUnits.MM, externalGearRatio=1.0):
+        super().__init__(lm, rm, g, wheelTravel, trackWidth, wheelBase, units, externalGearRatio)
