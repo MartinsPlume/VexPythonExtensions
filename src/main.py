be38339dc3bc9defc13576514ba75de18dfaec22
Silverwolf28 <48324152+Silverwolf28@users.noreply.github.com>
2024-04-18 17:46:06 +0300

    Added test app

---
+++ src/main.py
@@ -0,0 +1,16 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       Martins                                                      #
+# 	Created:      4/18/2024, 5:40:45 PM                                        #
+# 	Description:  IQ2 project                                                  #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello IQ2")
