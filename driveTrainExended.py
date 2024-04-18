d91d77c31967545027c44172a698b0c27898443f
Silverwolf28 <48324152+Silverwolf28@users.noreply.github.com>
2024-04-18 17:37:48 +0300

    Added Smart Drive extended

--- driveTrainExended.py
+++ driveTrainExended.py
@@ -1,19 +1,27 @@
 from vex import *
 
-def turn_slightly(self, direction, speed):
+class DriveTrainExtended(DriveTrain):
+    def __init__(self, lm, rm, wheelTravel=300, trackWidth=320, wheelBase=320,
+                 units=DistanceUnits.MM, externalGearRatio=1.0):
+        super().__init__(lm, rm, wheelTravel, trackWidth, wheelBase, units, externalGearRatio)
+
+    def turn_slightly(self, direction, speed):
         if direction == TurnType.LEFT:
             self.lm.spin(FORWARD, 100 - speed, PERCENT)
             self.rm.spin(FORWARD)
         elif direction == TurnType.RIGHT:
             self.rm.spin(FORWARD, 100 - speed, PERCENT)
             self.lm.spin(FORWARD)
-
-class DriveTrainExtended(DriveTrain):
-    def __init__(self, lm, rm, wheelTravel=300, trackWidth=320, wheelBase=320,
-                 units=DistanceUnits.MM, externalGearRatio=1.0):
-        super().__init__(lm, rm, wheelTravel, trackWidth, wheelBase, units, externalGearRatio)
     
 class SmartDriveExtended(SmartDrive):
     def __init__(self, lm, rm, g, wheelTravel=300, trackWidth=320, wheelBase=320,
                  units=DistanceUnits.MM, externalGearRatio=1.0):
         super().__init__(lm, rm, g, wheelTravel, trackWidth, wheelBase, units, externalGearRatio)
+
+    def turn_slightly(self, direction, speed):
+        if direction == TurnType.LEFT:
+            self.lm.spin(FORWARD, 100 - speed, PERCENT)
+            self.rm.spin(FORWARD)
+        elif direction == TurnType.RIGHT:
+            self.rm.spin(FORWARD, 100 - speed, PERCENT)
+            self.lm.spin(FORWARD)
